cbbe97afdf45c47bd747c3e452c8036ee4458400
Alexey Vassiliev <vaal12@yandex.ru>
2018-09-23 21:25:54 +0300

    1. Debug of error in legend 2. Changed info for Somalia, Western Sahara, Republic of congo 3. Changed Chechia, Czech republic in svg id to cz (as per ISO) 4. Added country populations table.

--- transformSVG.py
+++ transformSVG.py
@@ -8,14 +8,11 @@ def getNumberBucket(number, bucketsArray):
     return bucketIndex
 
 def transformCountrySVG(countriesListDF, bucketsArray, colorArray):
+    print("Buckets array:{}".format(bucketsArray))
     #Do not indicate parser - lxml will be used and this is fine as other produce some weird processing of the document.
     with open("world-map_20Sep2018.svg") as fp:
         #XML is needed as html.parser does something unspeakable to the code
         soup = BeautifulSoup(fp, features="xml") #, features="html5" "html.parser", "lxml"
-
-    # print("bucket array:"+str(bucketsArray))
-    # print("color array:{}".format(colorArray))
-
 
     country_buckets = []
     for rng in bucketsArray[1:]:
@@ -52,14 +49,8 @@ def transformCountrySVG(countriesListDF, bucketsArray, colorArray):
         for country in bucket:
             CSS_LINE += "."+country+", "
 
-        #BElow is to remove last comma
-        # print("Country list")
-        # print(CSS_LINE[:-2])
         CSS_LINE = CSS_LINE+" .legendclr"+str(colorIdx)+" {   fill:"+ colorArray[colorIdx]+";    } \n\n"
-        # CSS_LINE = CSS_LINE+" .legendclr"+str(colorIdx)+" { fill:"+ colorArray[colorIdx]+";    } \n\n"
         colorIdx +=1
-
-    # print("CSS:"+CSS_LINE)
 
     soup.svg.style.append(CSS_LINE)
 
@@ -69,7 +60,34 @@ def transformCountrySVG(countriesListDF, bucketsArray, colorArray):
     PLANK_HEIGHT = "50"
     PLANK_HEIGHT_INT = 50
 
-    for idx in range(0, len(colorArray)):
+    #adding plank for grey countries, which have not been coded
+    new_g_tag = soup.new_tag("g", transform="translate(280,"+
+                    str(baseline_y)+")")
+    new_rect_tag = soup.new_tag("rect",
+            rx="5", ry="5",
+            **{"width": PLANK_WIDTH,
+                "height" : PLANK_HEIGHT,
+                "stroke" : "green",
+                "class" : "landxx"
+            })
+
+    new_svg_tag = soup.new_tag("svg", width=PLANK_WIDTH,height=PLANK_HEIGHT)
+    new_text_tag = soup.new_tag("text", x="50%", y="50%",
+            **{
+                "text-anchor":"middle",
+                "alignment-baseline":"central",
+                "dominant-baseline":"central",
+                "font-family":"Verdana",
+                "font-size":"24"
+            })
+    new_text_tag.append("No data")
+
+    new_svg_tag.append(new_text_tag)
+    new_g_tag.append(new_rect_tag)
+    new_g_tag.append(new_svg_tag)
+    soup.svg.append(new_g_tag)
+
+    for idx in range(1, len(colorArray)+1):
         # print("Processing IDx:{}".format(idx))
         new_g_tag = soup.new_tag("g", transform="translate(280,"+
                         str(baseline_y+idx*(PLANK_HEIGHT_INT+20))+")")
@@ -78,7 +96,7 @@ def transformCountrySVG(countriesListDF, bucketsArray, colorArray):
                 **{"width": PLANK_WIDTH,
                     "height" : PLANK_HEIGHT,
                     "stroke" : "green",
-                    "class" : "legendclr"+str(idx)
+                    "class" : "legendclr"+str(idx-1)
                 })
 
         new_svg_tag = soup.new_tag("svg", width=PLANK_WIDTH,height=PLANK_HEIGHT)
@@ -91,10 +109,11 @@ def transformCountrySVG(countriesListDF, bucketsArray, colorArray):
                     "font-size":"24"
                 })
         #https://www.w3.org/wiki/Common_HTML_entities_used_for_typography
-        if idx != 0:
-            new_text_tag.append(u">"+str(bucketsArray[idx-1])+u" - ≤"+str(bucketsArray[idx]))
+        if idx == 1:
+            new_text_tag.append("≥{} - ≤{}".format(bucketsArray[idx-1], bucketsArray[idx])) #first bucket
         else:
-            new_text_tag.append(">0 - ≤"+str(bucketsArray[idx]))
+            new_text_tag.append(u">{} - ≤{}".format(bucketsArray[idx-1], bucketsArray[idx]))
+
 
         new_svg_tag.append(new_text_tag)
         new_g_tag.append(new_rect_tag)
