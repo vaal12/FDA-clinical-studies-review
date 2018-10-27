5711d321d39e055acc805a6d48107fb27eb518db
Alexey Vassiliev <vaal12@yandex.ru>
2018-10-27 23:18:54 +0300

    1. Animation on load is working. 2. Cleaned some helper CSS

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,21 @@
 require.config({
     paths: {
       d3: "../js/d3/d3.min",
-      // d3_dsv: "../js/d3/d3-dsv.v1.min",
-      // d3_fetch: "../js/d3/d3-fetch.v1.min"
     }
   });
   
   require(["d3"], function(d3) {
     d3.select("body").append("h1").text("Successfully loaded D3 version " + d3.version);
 
-    d3.svg("../svg/world-map.svg",
+    d3.svg("../svg/world-map_for_dashboard.svg",
       function(error, xml) {
-        alert("h1")
+        // alert("h1")
         if (error) throw error;
         
-        alert("Hello")
+        // alert("Hello")
     }).then(function(data) {
       // document.body.appendChild(data.documentElement);
-      alert("I am loaded")
+      // alert("I am loaded")
       importedNode = document.importNode(data.documentElement, true);
 
       // Load external SVG: https://bl.ocks.org/mbostock/1014829
@@ -26,9 +24,20 @@ require.config({
            this.appendChild(importedNode.cloneNode(true));
         }
       );
-      // d3.select("#countries_map").text("qwe3");
-      // console.log(data.documentElement)
+
+      //Transition of the first DIV:
+
+
+      d3.select("#left_container").transition().duration(1000)
+          .style("transform", "scale(0.01, 1)")
+          .on("end",
+            function(d) {
+              console.log("Hello")
+              d3.select("#country_details").text("Hello again");
+              d3.select("#country_graph").text("Hello again");
+            }
+          )
+          .transition().duration(1000)
+          .style("transform", "scale(1, 1)");
     });
-    
-    // alert("h2")
   });
