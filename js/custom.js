865ec95e9d650f709dc1cca0d84ebbb75f8bd07b
Alexey Vassiliev <vaal12@yandex.ru>
2018-10-28 03:54:11 +0300

    Working Quantile scale for coloring the map

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,14 @@ require.config({
       d3: "../js/d3/d3.min",
       populate_map: "../js/populate_map"
     },
-    shim: {
-      'd3': {
-          exports: 'd3',
-          init: function() {
-              window.d3 = d3;
-          }
-      },
-    }
+    // shim: {
+    //   'd3': {
+    //       exports: 'd3',
+    //       init: function() {
+    //           window.d3 = d3;
+    //       }
+    //   },
+    // }
   });
   
 require(["d3", "populate_map"], function(d3, populate_map) {
@@ -49,20 +49,30 @@ require(["d3", "populate_map"], function(d3, populate_map) {
       )
       .transition().duration(1000)
       .style("transform", "scale(1, 1)");
-    
-
-  // d3.csv("../csv/countries_trials_28Oct.csv").then(
-  //     function(data) {
-  //       console.log(data);
-  //       populate_map.populateMap("svg2985", data);
-  //     });//d3.csv("../csv/countries_trials_28Oct.csv").then (
-  // 
 
   d3.json("../csv/countries_trials_28Oct.json").then(
     function(data) {
       console.log(data);
-      populate_map.populateMap("svg2985", data);
-    });//d3.csv("../csv/countries_trials_28Oct.csv").then ()
-        // function(error, xml) {
+      var nct_counts_arr = []
+      for(var cnt in data) {
+        // console.log(cnt)
+        nct_counts_arr.push(data[cnt].nct_id_count)
+      }
+      nct_counts_arr.sort(function(a, b){return a - b});
+      // console.log("nct_counts_arr")
+      // console.log(nct_counts_arr);
+
+      //Chromatic scales: https://github.com/d3/d3-scale-chromatic
+      var quantile_scale = d3.scaleQuantile()
+          .domain(nct_counts_arr)
+          // .range( d3.schemeBlues[9]);
+          .range( d3.schemeYlGnBu[9]);
+          
+      console.log("Quantiles:")
+      console.log(quantile_scale.quantiles())
+      populate_map.populateMap("svg2985", data, quantile_scale);
+    });//d3.json("../csv/countries_trials_28Oct.json").then(
+          // function(data) {
+
   });//d3.svg("../svg/world-map_for_dashboard.svg",
 });//require(["d3"], function(d3) {
--- js/populate_map.js
+++ js/populate_map.js
@@ -2,44 +2,46 @@
 require.config({
     paths: {
       d3: "../js/d3/d3.min",
-      populate_map: "../js/populate_map"
+    //   populate_map: "../js/populate_map"
     }
   });
 
 
-// import * as d3 from 'd3';
 define(["d3"], function(d3){
     return {
-      name: 'David',
-      populateMap: function(svg_map_elem_id, data) {
-        console.log("Hello"+svg_map_elem_id);
+    //   name: 'David',
+      populateMap: function(svg_map_elem_id, data, quantile_scale) {
+        // console.log("Hello"+svg_map_elem_id);
         svg_elem = d3.select("#countries_map");
-        console.log(svg_elem);
+        // console.log(svg_elem);
         svg_elem.selectAll("g").each(
             function(d) {
-                // console.log("this:"+this);
                 this_tag_id = d3.select(this).attr("id") 
-                console.log(d3.select(this).attr("id"))
+                // console.log(d3.select(this).attr("id"))
                 // console.log(data[this_tag_id])
                 if(typeof data[this_tag_id] === 'undefined') {
-                    console.log("No data");
-                    
+                    // console.log("No data");
                 }
                 else {
                     console.log("   Data:")
                     console.log(data[this_tag_id].nct_id_count)
                     console.log(data[this_tag_id].full_country_name)
-
-                }
-                
-            }
+                    console.log(quantile_scale(
+                        data[this_tag_id].nct_id_count
+                    ));
+                    d3.select(this).selectAll("path").each(
+                        function() {
+                            d3.select(this).attr(
+                                "fill", 
+                                quantile_scale(data[this_tag_id].nct_id_count)
+                            );
+                        }
+                    );//d3.select(this).selectAll("path").each(
+                }//else {
+            }//svg_elem.selectAll("g").each(
+                // function(d) {    
         );
-        console.log(data)
-      }
-    }
-  });
-
-// requirejs(["d3"],
-    
-
-// )//requirejs
+        // console.log(data)
+      }//populateMap: function(svg_map_elem_id, data, quantile_scale) {
+    }//return {
+  });//define(["d3"], function(d3){
