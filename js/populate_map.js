efba3a6449e6176af8344737ca19462213ab386c
Alexey Vassiliev <vaal12@yandex.ru>
2018-10-31 11:47:41 +0200

    1. Actual json for country graph is loaded. 2. Cleaning of code.

--- js/populate_map.js
+++ js/populate_map.js
@@ -8,103 +8,110 @@ require.config({
     }
   });
 
+  function makeCountryChart(data, elementClicked) {
 
-function mapClicked(d3Obj, plotlyObj, elementClicked) {
-
-    d3.select("#left_container").transition().duration(1000)
-      .style("transform", "scale(0.001, 1)")
-      .on("end",
-        function(d) {
-          d3.select("#country_details").text("Hello again");
-          d3.select("#country_graph").text("");
-          var chart = c3.generate({
-            bindto: '#country_graph',
-            size: {
-                height: 140,
-                width: 280
-            },
-            data: {
-                x: "ticks",
-                columns: [
-                    [d3.select(elementClicked).attr("id"), 30, 200, 100, 400, 150, 250],
-                    ["ticks", '1999', '2000', "2001", "2002", "2003", "2004"],
-                ],
-                
-                labels: true,
-                type: 'bar'
-            },
-            legend: {
-                text:"I am a abar",
-                show: false,
-            },
-            axis: {
-                y: {
-                    show: false
-                },
-                x: {
-                    label : {
-                        text: "year",
-                        position: 'outer-right'
-                    },
-                    // type: 'timeseries',
-                    // tick: {
-                    //     // this also works for non timeseries data
-                    //     values: ['1999bis', '2000', "2001", "2002", "2003", "2004"]
-                    // }
-                }
-            },
-            tooltip: {
-                grouped: false,
-                position: function (data, width, height, element) {
-                    // console.log(element)
-                    // console.log(this);
-                    var chartOffsetX = document.querySelector("#country_graph").getBoundingClientRect().left;
-                    var graphOffsetX = document.querySelector("#country_graph g.c3-axis-y").getBoundingClientRect().right;
-                    // var tooltipWidth = document.getElementById('tooltip').parentNode.clientWidth;
-                    var tooltipWidth = 50;
-                    var x = (parseInt(element.getAttribute('cx')) ) + graphOffsetX - chartOffsetX - Math.floor(tooltipWidth/2);
-                    var y = element.getAttribute('cy');
-                    var y = y - height - 14;
-                    return {top: y, left: x}
-                },
-                // contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
-                //   d = Math.floor(d[0].value)
-                //   return '<div id="tooltip" class="module-triangle-bottom">' + d + ' wows</div>'
-                // }
-              }
-        });
-        }
-      )
-      .transition().duration(1000)
-      .style("transform", "scale(1, 1)");
-
+    console.log(d3.select(elementClicked).datum());
+    nct_ids = []
+    years = []
     
-    // console.log("mouse click:"+d3Obj.select(elementClicked).attr("id"))
+    for(year in data) {
+        // console.log(year)
+        years.push(year);
+        nct_ids.push(data[year].nct_id);
+    }
+    
+    // console.log(""+d3.max(years)+"  "+d3.min(years));
+    // console.log(years[0])
+    max_year = d3.max(years)
+    curr_year=d3.min(years);
+    i=0;
+    real_nct_ids = ["nct_id"];
+    real_years = ["ticks"]
+    while(curr_year<max_year) {
+        if(curr_year == years[i]) {
+            real_nct_ids.push(nct_ids[i])
+            real_years.push(years[i])
+            i++;
+        }
+        else {
+            real_nct_ids.push(0)
+            real_years.push(curr_year)
+        }
+        curr_year++;
+    }
+    d3.select("#country_graph").text("");
+    var chart = c3.generate({
+        bindto: '#country_graph',
+        size: {
+            height: 140,
+            width: 280
+        },
+        data: {
+            x: "ticks",
+            columns: [
+                real_nct_ids,
+                real_years,
+            ],
+            labels: true,
+            type: 'bar'
+        },
+        legend: {
+            text:"I am a abar",
+            show: false,
+        },
+        axis: {
+            y: {
+                show: false
+            },
+            x: {
+                label : {
+                    text: "year",
+                    position: 'outer-right'
+                },
+                // type: 'timeseries',
+                // tick: {
+                //     // this also works for non timeseries data
+                //     values: ['1999bis', '2000', "2001", "2002", "2003", "2004"]
+                // }
+            }
+        },
+        tooltip: {
+            grouped: false,
+            position: function (data, width, height, element) {
+                // console.log(element)
+                // console.log(this);
+                var chartOffsetX = document.querySelector("#country_graph").getBoundingClientRect().left;
+                var graphOffsetX = document.querySelector("#country_graph g.c3-axis-y").getBoundingClientRect().right;
+                // var tooltipWidth = document.getElementById('tooltip').parentNode.clientWidth;
+                var tooltipWidth = 50;
+                var x = (parseInt(element.getAttribute('cx')) ) + graphOffsetX - chartOffsetX - Math.floor(tooltipWidth/2);
+                var y = element.getAttribute('cy');
+                var y = y - height - 14;
+                return {top: y, left: x}
+            },
+            // contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+            //   d = Math.floor(d[0].value)
+            //   return '<div id="tooltip" class="module-triangle-bottom">' + d + ' wows</div>'
+            // }
+        }
+    });//var chart = c3.generate({
+  }//END function makeCountryChart(data) {
 
-    // var data = [{
-    //     x: ['giraffes', 'orangutans', 'monkeys'],
-    //     y: [20, 14, 23],
-    //     type: 'bar'
-    // }];
 
-    // var layout = {
-    //     title: 'Hide the Modebar',
-    //     showlegend: true,
-    //     autosize: false,
-    //     width: 340,
-    //     height: 220,
-    //     margin: {
-    //         l: 30,
-    //         r: 30,
-    //         b: 30,
-    //         t: 30,
-    //         pad: 4
-    //     },
-    //     // paper_bgcolor: '#7f7f7f',
-    //     // plot_bgcolor: '#c7c7c7'
-    // };
-      
-    // plotlyObj.newPlot('country_graph', data,  layout, {displayModeBar: false});
+function mapClicked(elementClicked) {
+    d3.select("#left_container").transition().duration(1000)
+        .style("transform", "scale(0.001, 1)")
+        .on("end", function(d) {
+            country_name = d3.select(elementClicked).datum().full_country_name;
+            d3.select("#country_details").text(country_name);
+            country_json = d3.json("../json/"+country_name+".json").then(function(data) {
+                console.log(data);
+                makeCountryChart(data, elementClicked);
+                d3.select("#left_container")
+                    .transition().duration(1000)
+                    .style("transform", "scale(1, 1)");
+            });//country_json = d3.json("../json/Albania.json").then(function(data) {
+        })//.on("end", function(d) {
 }//END function mapClicked(d3Obj, plotlyObj, elementClicked) {
 
 
@@ -122,12 +129,7 @@ define(["c3", "plotly"], function(c3, Plotly){ return {
                     // console.log("No data");
                 }//END  if(typeof data[this_tag_id] === 'undefined') {
                 else {// if(typeof data[this_tag_id] === 'undefined') {
-                    // console.log("   Data:")
-                    // console.log(data[this_tag_id].nct_id_count)
-                    // console.log(data[this_tag_id].full_country_name)
-                    // console.log(quantile_scale(
-                    //     data[this_tag_id].nct_id_count
-                    // ));
+                    
                     d3.select(this).selectAll("path, circle").each(
                         function() {
                             d3.select(this).attr(
@@ -144,18 +146,24 @@ define(["c3", "plotly"], function(c3, Plotly){ return {
                         data[this_tag_id].full_country_name+":  "+
                         data[this_tag_id].nct_id_count
                     )
+
+                    d3.select(this)
+                        .datum(data[this_tag_id])
+                        .on("click", function() {
+                            mapClicked(this)
+                        });
                 }//END else {// if(typeof data[this_tag_id] === 'undefined') {
             });//END svg_elem.selectAll("g").each(function(d) {
             
             //Removing extra circles, which do not have data available for them on the map
             svg_elem.selectAll(".circlexx").remove();
 
-            svg_elem.selectAll("g").each(
-                function() {
-                    d3.select(this).on("click", function() {
-                            mapClicked(d3, Plotly, this)
-                        }
-                    );
-            })//END svg_elem.selectAll("g").each(
+            // svg_elem.selectAll("g").each(
+            //     function() {
+            //         d3.select(this).on("click", function() {
+            //                 mapClicked(this)
+            //             }
+            //         );
+            // })//END svg_elem.selectAll("g").each(
         }//populateMap: function(svg_map_elem_id, data, quantile_scale) {
 } });//END define(["d3"], function(d3){ return {
