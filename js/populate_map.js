c8b47c8ad3182679c0caedc61cb47064162a40b4
Alexey Vassiliev <vaal12@yandex.ru>
2018-11-02 13:33:41 +0200

    Selector for map mode added. Debug of refactoring.

--- js/populate_map.js
+++ js/populate_map.js
@@ -149,7 +149,7 @@ define(["c3"], function (c3) { return {
             svg_elem.selectAll("g").each(function (d) {
                 this_tag_id = d3.select(this).attr("id")
                 // console.log(d3.select(this).attr("id"))
-                console.log(data[this_tag_id])
+                // console.log(data[this_tag_id])
                 if (typeof data[this_tag_id] === 'undefined') {
                     // console.log("No data");
                 }//END  if(typeof data[this_tag_id] === 'undefined') {
@@ -192,6 +192,13 @@ define(["c3"], function (c3) { return {
             // })//END svg_elem.selectAll("g").each(
         },//populateMap: function(svg_map_elem_id, data, quantile_scale) {
         prepareDataAndScale : function(data) {
+            //CREATE NEW DATA FIELD
+            for (var cnt in data) {
+                data[cnt].number_for_map_color = data[cnt].nct_id_count;
+                // console.log(cnt)
+                // console.log(data[cnt])
+            }
+
             var nct_counts_arr = []
             for (var cnt in data) {
               // console.log(cnt)
@@ -228,19 +235,55 @@ define(["c3"], function (c3) { return {
                 data: data
             }//return {
         },//prepareDataAndScale : function(data) {
-        colorLegend: function(good_quantiles)  {
+        colorLegend: function(dataAndScale)  {
             i = 0;
-            while (i < (good_quantiles.length - 1)) {
-                curr_color = quantile_scale(good_quantiles[i]);
-                legend_text = "" + Math.floor(good_quantiles[i]) + "-" +
-                (Math.floor(good_quantiles[i + 1]) - 1);
+            while (i < (dataAndScale.quantiles.length - 1)) {
+                curr_color = dataAndScale.scale(dataAndScale.quantiles[i]);
+                legend_text = "" + Math.floor(dataAndScale.quantiles[i]) + "-" +
+                (Math.floor(dataAndScale.quantiles[i + 1]) - 1);
     
                 // console.log("color:"+curr_color);
                 // console.log("Inverted:"+invertColor(curr_color))
                 d3.select("#legend" + (i + 1))
-                .attr("style", "background-color:" + curr_color + "; color:" + invertColor(curr_color))
-                .text(legend_text);
+                    .attr("style", "background-color:" + curr_color + "; color:" + invertColor(curr_color))
+                    .text(legend_text);
                 i++;
             };//while(i<(good_quantiles.length-1)) {
         },//colorLegend: function(good_quantiles)  {
 }});//END define(["d3"], function(d3){ return {
+
+
+//HELPER COLOR FUNCTIONS
+function invertColor(hex, bw) {
+    if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+      g = parseInt(hex.slice(2, 4), 16),
+      b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+      // http://stackoverflow.com/a/3943023/112731
+      return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+        ? '#000000'
+        : '#FFFFFF';
+    }
+    // invert color components
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+  }
+  
+  function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+  }
