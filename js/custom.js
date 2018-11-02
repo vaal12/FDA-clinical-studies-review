4dd071e4dfec36ec1f9600f8ce37eb970b04d560
Alexey Vassiliev <vaal12@yandex.ru>
2018-11-02 13:18:16 +0200

    1. Refactoring of javascript calls to allow for more modular approach to data handling.

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,36 @@
 require.config({
-    paths: {
-      d3: "../js/d3/d3.min",
-      populate_map: "../js/populate_map"
-    },
+  paths: {
+    d3: "../js/d3/d3.min",
+    populate_map: "../js/populate_map"
+  },
 });
-  
+
 function invertColor(hex, bw) {
-    if (hex.indexOf('#') === 0) {
-        hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
-    }
-    var r = parseInt(hex.slice(0, 2), 16),
-        g = parseInt(hex.slice(2, 4), 16),
-        b = parseInt(hex.slice(4, 6), 16);
-    if (bw) {
-        // http://stackoverflow.com/a/3943023/112731
-        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-            ? '#000000'
-            : '#FFFFFF';
-    }
-    // invert color components
-    r = (255 - r).toString(16);
-    g = (255 - g).toString(16);
-    b = (255 - b).toString(16);
-    // pad each with zeros and return
-    return "#" + padZero(r) + padZero(g) + padZero(b);
+  if (hex.indexOf('#') === 0) {
+    hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+    throw new Error('Invalid HEX color.');
+  }
+  var r = parseInt(hex.slice(0, 2), 16),
+    g = parseInt(hex.slice(2, 4), 16),
+    b = parseInt(hex.slice(4, 6), 16);
+  if (bw) {
+    // http://stackoverflow.com/a/3943023/112731
+    return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+      ? '#000000'
+      : '#FFFFFF';
+  }
+  // invert color components
+  r = (255 - r).toString(16);
+  g = (255 - g).toString(16);
+  b = (255 - b).toString(16);
+  // pad each with zeros and return
+  return "#" + padZero(r) + padZero(g) + padZero(b);
 }
 
 function padZero(str, len) {
@@ -39,75 +39,34 @@ function padZero(str, len) {
   return (zeros + str).slice(-len);
 }
 
-require(["d3", "populate_map"], function(d3, populate_map) {
+require(["d3", "populate_map"], function (d3, populate_map) {
   window.d3 = d3
-  d3.svg("../svg/world-map_for_dashboard.svg", function(error, xml) {
-      if (error) throw error;
-    }).then(function(data) {
-      // document.body.appendChild(data.documentElement);
-      // alert("I am loaded")
-      importedNode = document.importNode(data.documentElement, true);
+  d3.svg("../svg/world-map_for_dashboard.svg", function (error, xml) {
+    if (error) throw error;
+  }).then(function (data) {
+    // document.body.appendChild(data.documentElement);
+    // alert("I am loaded")
+    importedNode = document.importNode(data.documentElement, true);
 
-      // Load external SVG: https://bl.ocks.org/mbostock/1014829
-      d3.select("#countries_map").each(
-        function() {
-            this.appendChild(importedNode.cloneNode(true));
-      });//
+    // Load external SVG: https://bl.ocks.org/mbostock/1014829
+    d3.select("#countries_map").each(function () {
+        this.appendChild(importedNode.cloneNode(true));
+    });//d3.select("#countries_map").each(function () {
 
     d3.json("../csv/countries_trials_1Nov2018.json").then(
-      function(data) {
+      function (data) {
         console.log("countries_trials_1Nov2018.json")
         console.log(data);
-        var nct_counts_arr = []
-        for(var cnt in data) {
-          // console.log(cnt)
-          nct_counts_arr.push(data[cnt].nct_id_count)
-        }
-        nct_counts_arr.sort(function(a, b){return a - b});
-        console.log("nct_counts_arr")
-        console.log(nct_counts_arr);
+        console.log("Before")  
+        dataAndScale = populate_map.prepareDataAndScale(data);
+        console.log("scales:")
+        console.log(dataAndScale)
+        populate_map.populateMap("svg2985", data, dataAndScale.scale);
 
-        //Chromatic scales: https://github.com/d3/d3-scale-chromatic
-        var quantile_scale = d3.scaleQuantile()
-        // var quantile_scale = d3.scaleThreshold()
-        // var quantile_scale = d3.scaleQuantize()
-            .domain(nct_counts_arr)
-            // .range( d3.schemeBlues[9]);
-            .range( d3.schemeYlGnBu[9]);
-            
-        // console.log("Quantiles:")
-        // console.log(quantile_scale.quantiles())
-        populate_map.populateMap("svg2985", data, quantile_scale);
-
-        
-        good_quantiles = [0];
-        var i=0;
-        while(i<quantile_scale.quantiles().length) {
-          console.log(quantile_scale.quantiles()[i]);
-          good_quantiles.push(quantile_scale.quantiles()[i]);
-          i++;
-        }
-
-        good_quantiles.push(d3.max(nct_counts_arr));
-        // console.log("Good quantiles:"+good_quantiles);
-
-        i=0;
-        while(i<(good_quantiles.length-1)) {
-          curr_color = quantile_scale(good_quantiles[i]);
-          legend_text = ""+Math.floor(good_quantiles[i])+"-"+
-                    (Math.floor(good_quantiles[i+1])-1);
-                    
-          // console.log("color:"+curr_color);
-          // console.log("Inverted:"+invertColor(curr_color))
-          d3.select("#legend"+(i+1))
-            .attr("style", "background-color:"+curr_color+"; color:"+invertColor(curr_color))
-            .text(legend_text);
-          i++;
-        };//while(i<(good_quantiles.length-1)) {
+        populate_map.colorLegend(dataAndScale.quantiles)
 
         d3.select("#legend_table").attr("style", "");
-
       });//d3.json("../csv/countries_trials_28Oct.json").then(
-            // function(data) {
-    });//d3.svg("../svg/world-map_for_dashboard.svg", function(error, xml) {
+    // function(data) {
+  });//d3.svg("../svg/world-map_for_dashboard.svg", function(error, xml) {
 });//require(["d3", "populate_map"], function(d3, populate_map) {
--- js/populate_map.js
+++ js/populate_map.js
@@ -138,11 +138,12 @@ function mapClicked(elementClicked) {
 }//END function mapClicked(d3Obj, plotlyObj, elementClicked) {
 
 
-define(["c3"], function (c3) {
-    return {
+define(["c3"], function (c3) { return {
         populateMap: function (svg_map_elem_id, data, quantile_scale) {
             window.c3 = c3;
-            // console.log("Hello"+svg_map_elem_id);
+
+            // console.log
+            
             svg_elem = d3.select("#countries_map");
             // console.log(svg_elem);
             svg_elem.selectAll("g").each(function (d) {
@@ -189,6 +190,57 @@ define(["c3"], function (c3) {
             //             }
             //         );
             // })//END svg_elem.selectAll("g").each(
-        }//populateMap: function(svg_map_elem_id, data, quantile_scale) {
-    }
-});//END define(["d3"], function(d3){ return {
+        },//populateMap: function(svg_map_elem_id, data, quantile_scale) {
+        prepareDataAndScale : function(data) {
+            var nct_counts_arr = []
+            for (var cnt in data) {
+              // console.log(cnt)
+              nct_counts_arr.push(data[cnt].nct_id_count)
+            }
+            nct_counts_arr.sort(function (a, b) { return a - b });
+            // console.log("nct_counts_arr")
+            // console.log(nct_counts_arr);
+
+            //GENERATE SCALES
+            //Chromatic scales: https://github.com/d3/d3-scale-chromatic
+            var quantile_scale = d3.scaleQuantile()
+            // var quantile_scale = d3.scaleThreshold()
+            // var quantile_scale = d3.scaleQuantize()
+                .domain(nct_counts_arr)
+                // .range( d3.schemeBlues[9]);
+                .range( d3.schemeYlGnBu[9]);
+
+            //CREATE QUANTILES ARRAY
+            good_quantiles = [0];
+            var i = 0;
+            while (i < quantile_scale.quantiles().length) {
+                // console.log(quantile_scale.quantiles()[i]);
+                good_quantiles.push(quantile_scale.quantiles()[i]);
+                i++;
+            }
+    
+            good_quantiles.push(d3.max(nct_counts_arr));
+            // console.log("Good quantiles:"+good_quantiles);
+
+            return {
+                scale: quantile_scale, 
+                quantiles: good_quantiles,
+                data: data
+            }//return {
+        },//prepareDataAndScale : function(data) {
+        colorLegend: function(good_quantiles)  {
+            i = 0;
+            while (i < (good_quantiles.length - 1)) {
+                curr_color = quantile_scale(good_quantiles[i]);
+                legend_text = "" + Math.floor(good_quantiles[i]) + "-" +
+                (Math.floor(good_quantiles[i + 1]) - 1);
+    
+                // console.log("color:"+curr_color);
+                // console.log("Inverted:"+invertColor(curr_color))
+                d3.select("#legend" + (i + 1))
+                .attr("style", "background-color:" + curr_color + "; color:" + invertColor(curr_color))
+                .text(legend_text);
+                i++;
+            };//while(i<(good_quantiles.length-1)) {
+        },//colorLegend: function(good_quantiles)  {
+}});//END define(["d3"], function(d3){ return {
